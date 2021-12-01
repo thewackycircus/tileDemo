e6e92fb5e909819a85107a7bea94bbf575ee69b4
Sean Jones <thewackycircus@gmail.com>
2021-12-01 18:56:19 +0000

    added items. BuildWorld() renders the tiles and then renders items on top so that tile can still be seen underneath

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 let world = {
     ROWS: 10,
     COLUMNS: 10,
-    TILEWIDTH: 32, // in px
+    TILESIZE: 32, // in px
 
     // this array represents the tile layout
     l1Surface_ary: [
@@ -16,6 +16,20 @@ let world = {
         [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [0, 0, 1, 0, 0, 0, 0, 1, 0, 0]
+    ],
+
+    // this array represents the item posistions
+    l1Item_ary: [
+        [0, 0, 0, 3, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 3, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
     ]
 };
 
@@ -23,8 +37,8 @@ let world = {
 let config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: world.COLUMNS * world.TILEWIDTH,
-    height: world.ROWS * world.TILEWIDTH,
+    width: world.COLUMNS * world.TILESIZE,
+    height: world.ROWS * world.TILESIZE,
     scene: {
         preload: preload,
         create: create
@@ -33,6 +47,7 @@ let config = {
 
 function preload() {
     this.load.atlasXML('tiles', 'assets/tiles.png', 'assets/tiles.xml'); // loading tile images as a tile map
+    this.load.atlasXML('items', 'assets/items.png', 'assets/items.xml'); // loading item images as a tile map
 }
 
 function create() {
@@ -44,10 +59,24 @@ function buildWorld(scene, world) {
         for (let colIdx = 0; colIdx < world.COLUMNS; colIdx++) {
             // returns tile0000 or tile0001 by using the above tile layout array. This relates to the tile labelled in the tile.xml file
             let tileStr = "tile000" + world.l1Surface_ary[rowIdx][colIdx];
-            test = scene.add.sprite(
-                colIdx * world.TILEWIDTH + world.TILEWIDTH/2, // positioning for tile has to offset because pivot point is in center of tile
-                rowIdx * world.TILEWIDTH + world.TILEWIDTH/2, 
-                'tiles', tileStr);
+            scene.add.sprite(
+                colIdx * world.TILESIZE + world.TILESIZE/2, // positioning for tile has to offset because pivot point is in center of tile
+                rowIdx * world.TILESIZE + world.TILESIZE/2, 
+                'tiles', tileStr
+            );
+            
+            // adding items on top of tiles
+            itemNum = world.l1Item_ary[rowIdx][colIdx];
+            // allows 0 to represent no items
+            if (itemNum !== 0) {
+                // works the same way as tiles above
+                let itemStr = "item000" + itemNum;
+                scene.add.sprite(
+                    colIdx * world.TILESIZE + world.TILESIZE/2,
+                    rowIdx * world.TILESIZE + world.TILESIZE/2,
+                    'items', itemStr
+                );
+            }
         }
     }
 }
