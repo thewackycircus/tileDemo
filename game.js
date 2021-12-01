7ea82a426d94f31e2ad52e17451ed1c1061f43b9
Sean Jones <thewackycircus@gmail.com>
2021-12-01 20:06:08 +0000

    added a player and movement using cursor keys

--- game.js
+++ game.js
@@ -30,7 +30,10 @@ let world = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [1, 0, 0, 0, 3, 0, 0, 0, 0, 0],
         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    ]
+    ],
+
+    // player container
+    player_spr: null
 };
 
 // normal phaser config
@@ -41,17 +44,32 @@ let config = {
     height: world.ROWS * world.TILESIZE,
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 function preload() {
-    this.load.atlasXML('tiles', 'assets/tiles.png', 'assets/tiles.xml'); // loading tile images as a tile map
-    this.load.atlasXML('items', 'assets/items.png', 'assets/items.xml'); // loading item images as a tile map
+    // loading asset atlasses
+    this.load.atlasXML('tiles', 'assets/tiles.png', 'assets/tiles.xml');
+    this.load.atlasXML('items', 'assets/items.png', 'assets/items.xml');
+    this.load.atlasXML('player', 'assets/newPlayer.png', 'assets/newPlayer.xml');
+
 }
 
 function create() {
+
     buildWorld(this, world);
+
+    // using functions from convert.js in libs folder to position player
+    world.player_spr = this.add.sprite(
+        colToXPx(world, 7) + world.TILESIZE/2, // setting x position to column 7
+        rowToYPx(world, 9) + world.TILESIZE/2, // setting y position to row 9
+        'player'
+    );
+
+    // creating event listeners for arrow keys
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function buildWorld(scene, world) {
@@ -79,6 +97,32 @@ function buildWorld(scene, world) {
             }
         }
     }
+}
+
+function update() {
+    // getting current player position
+    xPos = world.player_spr.x;
+    yPos = world.player_spr.y;
+
+    // check for movement keys
+    if (cursors.left.isDown) {
+        world.player_spr.setFrame("newPlayer0001");
+        xPos -= 2;
+    } else if (cursors.right.isDown) {
+        world.player_spr.setFrame("newPlayer0002");
+        xPos += 2;
+    } else if (cursors.down.isDown) {
+        world.player_spr.setFrame("newPlayer0003");
+        yPos += 2;
+    } else if (cursors.up.isDown) {
+        world.player_spr.setFrame("newPlayer0004");
+        yPos -= 2;
+    } else {
+        world.player_spr.setFrame("newPlayer0000");
+    }
+
+    world.player_spr.x = xPos;
+    world.player_spr.y = yPos;
 }
 
 let game = new Phaser.Game(config);
--- libs/convert.js
+++ libs/convert.js
@@ -7,7 +7,7 @@ function xPxToCol(world, x) {
     if (x < 1) {
         col = 0;
     } else {
-        col = (Math.floor(x / world.TILEWIDTH));
+        col = (Math.floor(x / world.TILESIZE));
     }
     if (col >= world.COLUMNS) {
         col = world.COLUMNS-1;
@@ -20,7 +20,7 @@ function yPxToRow(world, y) {
     if (y < 1) {
         row = 0;
     } else {
-        row = (Math.floor(y / world.TILEWIDTH));
+        row = (Math.floor(y / world.TILESIZE));
     }
     if (row >= world.ROWS) {
         row = world.ROWS - 1;
@@ -28,13 +28,13 @@ function yPxToRow(world, y) {
     return (row);
 }
 
-function colToXpx(world, col) {
+function colToXPx(world, col) {
     var x;
-    var maxPx = Math.floor(world.COLUMNS * world.TILEWIDTH);
+    var maxPx = Math.floor(world.COLUMNS * world.TILESIZE);
     if (col < 1) {
         x = 0;
     } else {
-        x = (Math.floor(col * world.TILEWIDTH));
+        x = (Math.floor(col * world.TILESIZE));
         if (x > maxPx) {
             x = maxPx;
         }
@@ -44,11 +44,11 @@ function colToXpx(world, col) {
 
 function rowToYPx(world, row) {
     var y;
-    var maxPx = Math.floor(world.ROWS * world.TILEWIDTH);
+    var maxPx = Math.floor(world.ROWS * world.TILESIZE);
     if (row < 1) {
         y = 0;
     } else {
-        y = (Math.floor(row * world.TILEWIDTH));
+        y = (Math.floor(row * world.TILESIZE));
         if (y > maxPx) {
             y = maxPx;
         }
